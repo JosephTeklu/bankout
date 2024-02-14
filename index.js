42ec82a5995e1acbef5033a4d771d08e88a3987b
Joseph Teklu <jteklu@outlook.com>
2024-02-14 16:46:23 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require("./utils/generateMarkdown.js");
+const generateMarkdown = require('./utils/generateMD');
+
+
